b294e1d93f7cc038f8fa719aad1b707d46af0229
Frans Johansson <fransjohansson98@gmail.com>
2020-09-16 17:03:49 +0200

    Fix wall triangles and render simple png image

--- src/craytrace.cpp
+++ src/craytrace.cpp
@@ -8,9 +8,9 @@ glm::vec3 Triangle::rayIntersection(const Ray& ray) {
         vec3 pe = ray.end.xyz();
 
         vec3 T = ps - this->v1.xyz();
-        vec3 E1 = this->v2.xyz() - this->v2.xyz();
+        vec3 E1 = this->v2.xyz() - this->v1.xyz();
         vec3 E2 = this->v3.xyz() - this->v1.xyz();
-        vec3 D = ps-pe;
+        vec3 D = pe-ps;
         vec3 P = cross(D, E2);
         vec3 Q = cross(T, E1);
 
@@ -21,20 +21,58 @@ glm::vec3 Triangle::rayIntersection(const Ray& ray) {
 
 // SCENE
 
-void Scene::rayIntersection(Ray &ray) {
-    for (Triangle triangle : walls) {
+void Scene::rayIntersection(Ray &ray) const {
+    for (Triangle triangle : this->walls) {
         glm::vec3 hit = triangle.rayIntersection(ray);
+        // std::cout << "u = " << hit.y << " v = " << hit.z << '\n';
         // TODO: Handle a potentially bad hit
-        if (hit.y >= 0 && hit.z >= 0 && hit.y + hit.z <= 1) {
+        if (hit.y >= 0.0f && hit.z >= 0.0f && hit.y + hit.z <= 1.0f) {
             ray.end = (1.0f - hit.y - hit.z)*triangle.v1 + hit.y*triangle.v2 + hit.z*triangle.v3;
-            ray.target = triangle;   
+            ray.target = std::make_shared<Triangle>(triangle);
+            return;
         }
     }
 } 
 
 // CAMERA
 
-void render()
+void Camera::render(const Scene& scene, const char* filename)
 {
-    // TODO: Do raytracing here! 😺
+    png::image<png::rgb_pixel> image(IMAGE_SIZE, IMAGE_SIZE);
+    for (size_t y = 0; y < image.get_height(); ++y)
+    {
+        for (size_t x = 0; x < image.get_width(); ++x)
+        {
+            // TODO: Do raytracing here! 😺
+            // Spawn a new ray going through the pixel
+            glm::vec4 start = this->primaryEyeActive ? this->primaryEye : this->secondaryEye;
+            glm::vec4 end = glm::vec4(0.0, ((float)x-401.0f+0.5f) * PIXEL_SIZE, ((float)y-401.0f+0.5f) * PIXEL_SIZE, 1.0);
+
+            Ray ray = Ray{ start, end, nullptr, Color{ 0.0, 0.0, 0.0 }, 1.0 };
+            
+            // Find the intersecting triangle in the scene
+            scene.rayIntersection(ray);
+
+            // Get the color of the ray as R, G, and B
+            png::byte r, g, b;
+            if (ray.target) {
+                r = ray.target->color.r * 255;
+                g = ray.target->color.g * 255;
+                b = ray.target->color.b * 255;
+            } else {
+                std::cout << "Ray did not have a target triangle\n";
+                r = 255;
+                g = 0;
+                b = 255;
+            }
+
+            // Output color to image
+            image[y][x] = png::rgb_pixel(r, g, b);
+        }
+    }
+    image.write(filename);
+}
+
+void Camera::toggleActiveEye() {
+    this->primaryEyeActive = !this->primaryEyeActive;
 }
--- src/craytrace.hpp
+++ src/craytrace.hpp
@@ -2,9 +2,13 @@
 
 #include <iostream>
 #include <vector>
+#include <memory>
 #define GLM_FORCE_SWIZZLE
-#include "../dependencies/glm/glm.hpp"
+#include <glm/glm.hpp>
+#include <png++/png.hpp>
 
+const int IMAGE_SIZE = 800;
+const float PIXEL_SIZE = 2.0f / IMAGE_SIZE;
 
 struct Ray;
 
@@ -15,13 +19,13 @@ struct Color {
 class Triangle { 
 private:
     glm::vec3 normal;
-    Color color;
 
 public:
     glm::vec4 v1, v2, v3;
+    Color color;
 
-    Triangle(glm::vec4 v1, glm::vec4 v2, glm::vec4 v3, Color color)
-        : v1{v1}, v2{v2}, v3{v3}, color{color}
+    Triangle(glm::vec4 _v1, glm::vec4 _v2, glm::vec4 _v3, Color _color)
+        : v1{_v1}, v2{_v2}, v3{_v3}, color{_color}
     {
         this->normal = glm::cross((v2.xyz()-v1.xyz()), (v3.xyz()-v1.xyz()));
     } 
@@ -31,7 +35,7 @@ public:
 
 struct Ray {
     glm::vec4 start, end;
-    Triangle target;
+    std::shared_ptr<Triangle> target;
     Color color;
     double importance;    
 };
@@ -47,22 +51,24 @@ private:
     std::vector<Triangle> walls;
 
 public:
-    Scene(std::vector<Triangle> walls)
-    : walls{walls} { }
+    Scene(std::vector<Triangle> _walls)
+    : walls{_walls} { }
 
-    void rayIntersection(Ray &ray);
+    void rayIntersection(Ray &ray) const;
 };
 
 class Camera {
 private:
     glm::vec4 primaryEye, secondaryEye;
     bool primaryEyeActive;
-    Pixel image [800][800];
+    // Pixel image [IMAGE_SIZE][IMAGE_SIZE];
 
 public:
-    Camera(glm::vec4 pridmaryEye, glm::vec4 secondaryEye) 
-    : primaryEye{primaryEye}, secondaryEye{secondaryEye}, primaryEyeActive{true} {}
+    Camera(glm::vec4 _primaryEye, glm::vec4 _secondaryEye) 
+    : primaryEye{_primaryEye}, secondaryEye{_secondaryEye}, primaryEyeActive{true} {}
     
-    void render();
+    void render(const Scene&, const char* filename);
+
+    void toggleActiveEye();
 };
 
